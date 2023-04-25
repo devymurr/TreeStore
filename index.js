a4e09dee5256820d678c7da933802bbf77b4c5b0
devymurr <devymurr@gmail.com>
2023-04-25 18:33:35 +0700

    TreeStore

---
+++ index.js
@@ -0,0 +1,75 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var TreeStore = /** @class */ (function () {
+    function TreeStore(items) {
+        var _this = this;
+        this._itemsMap = new Map();
+        this._parentMap = new Map();
+        this._items = items;
+        items.forEach(function (item) {
+            _this._itemsMap.set(item.id, item);
+            _this._parentMap.set(item.parent, __spreadArray(__spreadArray([], (_this._parentMap.get(item.parent) || []), true), [
+                item,
+            ], false));
+        });
+    }
+    TreeStore.prototype.getAll = function () {
+        return this._items;
+    };
+    TreeStore.prototype.getItem = function (id) {
+        return this._itemsMap.get(id);
+    };
+    TreeStore.prototype.getChildren = function (id) {
+        return this._parentMap.get(id) || [];
+    };
+    TreeStore.prototype.getAllChildren = function (id) {
+        var rootChildren = this.getChildren(id);
+        var allChildren = rootChildren || [];
+        var stack = __spreadArray([], rootChildren, true);
+        while (stack.length) {
+            var item = stack.pop();
+            if (this._parentMap.has(item.id)) {
+                allChildren.push.apply(allChildren, (this._parentMap.get(item.id) || []));
+            }
+        }
+        return allChildren;
+    };
+    TreeStore.prototype.getAllParents = function (id) {
+        var node = this.getItem(id);
+        var parents = [];
+        while (node) {
+            parents.push(node);
+            node = this.getItem(node === null || node === void 0 ? void 0 : node.parent);
+        }
+        return parents;
+    };
+    return TreeStore;
+}());
+var items = [
+    { id: 1, parent: 'root' },
+    { id: 2, parent: 1, type: 'test' },
+    { id: 3, parent: 1, type: 'test' },
+    { id: 4, parent: 2, type: 'test' },
+    { id: 5, parent: 2, type: 'test' },
+    { id: 6, parent: 2, type: 'test' },
+    { id: 7, parent: 4, type: null },
+    { id: 8, parent: 4, type: null },
+];
+var ts = new TreeStore(items);
+// --- test
+console.log(ts.getAll());
+console.log(ts.getItem(4));
+console.log(ts.getChildren(2));
+console.log(ts.getAllChildren(2));
+console.log(ts.getAllParents(4));
+console.log(ts.getItem(99));
+console.log(ts.getChildren(99));
+console.log(ts.getAllChildren(99));
+console.log(ts.getAllParents(99));
---
+++ index.ts
@@ -0,0 +1,99 @@
+class TreeStore implements TreeStoreFactory {
+  readonly _items: Item[]
+  readonly _itemsMap: Map<ID, Item> = new Map()
+  readonly _parentMap: Map<ID, Item[]> = new Map()
+
+  constructor(items: Item[]) {
+    this._items = items
+
+    items.forEach((item) => {
+      this._itemsMap.set(item.id, item)
+      this._parentMap.set(item.parent, [
+        ...(this._parentMap.get(item.parent) || []),
+        item,
+      ])
+    })
+  }
+  getAll() {
+    return this._items
+  }
+
+  getItem(id: ID) {
+    return this._itemsMap.get(id)
+  }
+
+  getChildren(id: ID) {
+    return this._parentMap.get(id) || []
+  }
+
+  getAllChildren(id: ID) {
+    const rootChildren = this.getChildren(id)
+    const allChildren = rootChildren || []
+
+    const stack = [...rootChildren]
+
+    while (stack.length) {
+      const item = stack.pop()
+      if (this._parentMap.has(item!.id)) {
+        allChildren.push(...(this._parentMap.get(item!.id) || []))
+      }
+    }
+
+    return allChildren
+  }
+
+  getAllParents(id: ID) {
+    let node = this.getItem(id)
+    const parents: Item[] = []
+
+    while (node) {
+      parents.push(node)
+      node = this.getItem(node?.parent)
+    }
+
+    return parents
+  }
+}
+
+const items = [
+  { id: 1, parent: 'root' },
+  { id: 2, parent: 1, type: 'test' },
+  { id: 3, parent: 1, type: 'test' },
+
+  { id: 4, parent: 2, type: 'test' },
+  { id: 5, parent: 2, type: 'test' },
+  { id: 6, parent: 2, type: 'test' },
+
+  { id: 7, parent: 4, type: null },
+  { id: 8, parent: 4, type: null },
+]
+const ts = new TreeStore(items)
+
+// --- test
+console.log(ts.getAll())
+console.log(ts.getItem(4))
+console.log(ts.getChildren(2))
+console.log(ts.getAllChildren(2))
+console.log(ts.getAllParents(4))
+
+console.log(ts.getItem(99))
+console.log(ts.getChildren(99))
+console.log(ts.getAllChildren(99))
+console.log(ts.getAllParents(99))
+
+// ---- typing
+type ID = number | string
+type Nullable<T> = T | null | undefined
+type Item = {
+  id: ID
+  parent: ID
+  type?: any
+}
+interface TreeStoreFactory {
+  _items: Item[]
+  getAll: () => Item[]
+  getItem: (id: ID) => Nullable<Item>
+  getChildren: (id: ID) => Array<Item | null>
+  getAllChildren: (id: ID) => Item[]
+  getAllParents: (id: ID) => Item[]
+}
